afb373d0525857ff65ff90cf83dd10fc27db87db
Simon Hong <shong@igalia.com>
2019-07-05 21:28:14 +0900

    Add always show bookmark bar on NTP option
    Some user might want to show bookmark on NTP
    when show bookmark bar option is false.

--- common/pref_names.cc
+++ common/pref_names.cc
@@ -57,3 +57,5 @@ const char kNewTabPageShowBackgroundImage[] =
 const char kNewTabPageShowClock[] = "brave.new_tab_page.show_clock";
 const char kNewTabPageShowTopSites[] = "brave.new_tab_page.show_top_sites";
 const char kNewTabPageShowStats[] = "brave.new_tab_page.show_stats";
+const char kAlwaysShowBookmarkBarOnNTP[] =
+    "brave.always_show_bookmark_bar_on_ntp";
